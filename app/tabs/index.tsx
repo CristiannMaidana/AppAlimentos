7305f298e0e5256a045d4e29b07b69fd92bc183f
Cristian <cristian.mg.david@gmail.com>
2026-04-21 21:06:41 -0300

    feat(index): add texts, change the style of the Sheet

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>CURATED FLAVORS</Text>
+      <Text style={{ fontSize: 30, marginTop: 16, fontWeight: 'bold' }}>
+        The art of conscious discovery.
+      </Text>
     </View>
   );
 }
@@ -11,8 +14,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#673d3d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    padding: 16,
+    },
 });
