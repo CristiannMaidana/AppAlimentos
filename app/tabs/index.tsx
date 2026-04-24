9d56dae110b92a2fce361d1128c29d58af1af7e5
Cristian <cristian.mg.david@gmail.com>
2026-04-24 20:59:24 -0300

    feat(index): create list of cards, add cards to the page

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,6 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import CustomCard from '../components/details_categories';
 
 export default function HomeScreen() {
+  const cards = [
+
+  {
+
+    id: '1',
+
+    title: 'Pizza',
+
+    image: 'https://via.placeholder.com/100',
+
+    backgroundColor: '#fdd835',
+
+  },
+
+  {
+
+    id: '2',
+
+    title: 'Burger',
+
+    image: 'https://via.placeholder.com/100',
+
+    backgroundColor: '#81c784',
+
+  },
+
+  {
+
+    id: '3',
+
+    title: 'Pasta',
+
+    image: 'https://via.placeholder.com/100',
+
+    backgroundColor: '#64b5f6',
+
+  },
+
+  {
+
+    id: '4',
+
+    title: 'Salad',
+
+    image: 'https://via.placeholder.com/100',
+
+    backgroundColor: '#ff8a65',
+
+  },
+
+];
   return (
     <View style={styles.container}>
       <Text>CURATED FLAVORS</Text>
@@ -10,6 +62,31 @@ export default function HomeScreen() {
       <Text style={{ fontSize: 20, marginTop: 32,}}>
         Categories
        </Text>
+       <FlatList
+
+        data={cards}
+
+        keyExtractor={(item) => item.id}
+
+        numColumns={2}
+
+        renderItem={({ item }) => (
+
+          <CustomCard
+
+            title={item.title}
+
+            image={item.image}
+
+            backgroundColor={item.backgroundColor}
+
+            onPress={() => alert(item.title)}
+
+          />
+
+        )}
+
+      />
     </View>
   );
 }
