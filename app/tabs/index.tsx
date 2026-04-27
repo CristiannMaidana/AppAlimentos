86371e274dc5457d149dae524abfa022ff74eb54
Cristian <cristian.mg.david@gmail.com>
2026-04-27 18:14:23 -0300

    feat(index): create view,for a row wrap content of buttons, add Pressable and Text for style buttons. Create style of button and textButton

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, ScrollView, StyleSheet, Text } from 'react-native';
+import { FlatList, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import CustomCard from '../components/details_categories';
 
 export default function HomeScreen() {
@@ -92,6 +92,39 @@ export default function HomeScreen() {
         Refine by Taste
       </Text>
 
+      <View style={{ marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}> 
+        <Pressable style={styles.button} onPress={() => alert('Organic button pressed')}>
+          <Text style={styles.textButton}>Organic</Text>
+        </Pressable>  
+        <Pressable style={styles.button} onPress={() => alert('Vegan button pressed')}>
+          <Text style={styles.textButton}>Vegan</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('Vegetarian button pressed')}>
+          <Text style={styles.textButton}>Vegetarian</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('Gluten-free button pressed')}>
+          <Text style={styles.textButton}>Gluten-free</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('no-added-sugar button pressed')}>
+          <Text style={styles.textButton}>no-added-sugar</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('fair-trade button pressed')}>
+          <Text style={styles.textButton}>fair-trade</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('lactose-free button pressed')}>
+          <Text style={styles.textButton}>lactose-free</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('palm-oil-free button pressed')}>
+          <Text style={styles.textButton}>palm-oil-free</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('low-fat button pressed')}>
+          <Text style={styles.textButton}>high-fiber</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert('low-fat button pressed')}>
+          <Text style={styles.textButton}>low-fat</Text>
+        </Pressable>
+      </View>
+
     </ScrollView>
   );
 }
@@ -102,5 +135,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'stretch',
     padding: 20,
-    },
+  },
+  button: {
+    margin: 10,
+    backgroundColor: '#6fda7d',
+    paddingHorizontal: 25,
+    paddingVertical: 15,
+    borderRadius: 25,
+    justifyContent: 'center',
+  },
+  textButton: {
+    color: '#000000',
+    fontSize: 16,
+  },
 });
