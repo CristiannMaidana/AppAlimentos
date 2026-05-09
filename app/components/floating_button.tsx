2515368d69bd22d05254720e5a5255ed1e90cc7c
Cristian <cristian.mg.david@gmail.com>
2026-05-09 14:36:47 -0300

    feat(floating_button): create component
    - Create floating button for search
    
    - Add constructor of propieties, style and method of action button

---
+++ app/components/floating_button.tsx
@@ -0,0 +1,40 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Pressable, StyleSheet } from 'react-native';
+
+
+type FloatingButtonProps = {
+    iconName: keyof typeof Ionicons.glyphMap;
+    backgroundColor: string;
+    onPress: () => void;
+};
+
+export default function FloatingButton({
+    iconName,
+    backgroundColor,
+    onPress,
+}: FloatingButtonProps) {
+    return (
+    <Pressable onPress={onPress} style={[styles.button, { backgroundColor }]}>
+      <Ionicons name={iconName} size={24} color="white" />
+    </Pressable>
+  );
+}
+
+// Styles for the FloatingButton component
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84, 
+  },
+});
