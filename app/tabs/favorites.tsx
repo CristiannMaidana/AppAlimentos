9fb9cac1f206098c7615c8fd68d98a2339457e41
Cristian <cristian.mg.david@gmail.com>
2026-05-10 16:16:46 -0300

    feat(favorites): new page
    - Add ScrollView to make the page scrollable

---
+++ app/tabs/favorites.tsx
@@ -0,0 +1,9 @@
+import { ScrollView } from 'react-native';
+
+export default function FavoritesScreen() {
+    return (
+        <ScrollView style={{ backgroundColor: '#f7f8f9', padding: 20 }}>
+            
+        </ScrollView>
+    );
+}
