d322894d7b8d4a4322bf67641b6abaf21718f2a0
Cristian <cristian.mg.david@gmail.com>
2026-05-12 19:42:38 -0300

    feat(values_nutritional): create component to show the nutrie values of the product selected

---
+++ app/components/values_nutritional.tsx
@@ -0,0 +1,23 @@
+
+type ValuesNutritionalProps = {
+    energy: string;
+    fat: string;
+    saturatedFat: string;
+    carbohydrates: string;
+    sugars: string;
+    fiber: string;
+    proteins: string;
+    salt: string;
+}
+
+export default function ValuesNutritional({
+    energy, 
+    fat, 
+    saturatedFat, 
+    carbohydrates, 
+    sugars, 
+    fiber, 
+    proteins, 
+    salt}: ValuesNutritionalProps) {
+    return 
+};
