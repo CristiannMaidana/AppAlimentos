1950c56e721fd6136185fcb312b75a3cf64cf130
Cristian <cristian.mg.david@gmail.com>
2026-05-15 16:18:17 -0300

    feat(_layout): add buttoms in header

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -1,5 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Alert, Pressable } from 'react-native';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
@@ -14,7 +16,22 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: true,
-        headerTitle: 'DigitalEpicurean',
+        headerTitle: 'Digital Epicurean',
+        headerTitleAlign: 'center',
+        headerLeft: () => (
+          <Pressable
+            onPress={() => Alert.alert('Options', 'Options button touched')}
+            style={{ marginLeft: 16 }}>
+            <MaterialIcons name="menu" size={28} color="#000000" />
+          </Pressable>
+        ),
+        headerRight: () => (
+          <Pressable
+            onPress={() => Alert.alert('User', 'User button touched')}
+            style={{ marginRight: 16 }}>
+            <MaterialIcons name="account-circle" size={28} color="#000000" />
+          </Pressable>
+        ),
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
