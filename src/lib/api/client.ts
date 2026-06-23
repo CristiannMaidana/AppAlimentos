f6c76092ea73f472dcf48fc10cbe0e9205493ea4
Cristian <cristian.mg.david@gmail.com>
2026-06-23 17:17:47 -0300

    feat(client): create promise for conexion with API

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,1 +1,9 @@
-export {};
+export async function apiGet<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+
+    if (!response.ok){
+        throw new Error('Request faild');
+    }
+    else 
+        return response.json();
+};
