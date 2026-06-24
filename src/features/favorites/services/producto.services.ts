a2e23b6113003045b873357748f0a7e0dcf030a5
Cristian <cristian.mg.david@gmail.com>
2026-06-24 20:03:19 -0300

    feat(services): add promise to the function

--- src/features/favorites/services/producto.services.ts
+++ src/features/favorites/services/producto.services.ts
@@ -1,12 +1,12 @@
 import { apiGet } from '@/lib/api/client';
 
-const url = 'api/v2/product/';
-const query = '?fields=product_name,nova_group,nutriscore_grade,ecoscore_grade,nutriments,selected_images,ingredients_text';
+const url = 'api/v2/product';
+const query = '?fields=code,product_name,nova_group,nutriscore_grade,ecoscore_grade,nutriments,selected_images,ingredients_text,nutrition_data_per';
 
-function builUrl(code: number): string {
-  return `${url}${code}${query}`
-};
+function buildUrl(code: number): string {
+  return `${url}/${code}${query}`;
+}
 
-export async function fetchProduct(code: number) {
-  return apiGet<any>(builUrl(code));
+export async function fetchProduct(code: number): Promise<any> {
+  return apiGet<any>(buildUrl(code));
 }
