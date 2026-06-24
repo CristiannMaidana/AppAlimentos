a7d3edc4c53c7f22ee4df0ebd572fee34bf01f79
Cristian <cristian.mg.david@gmail.com>
2026-06-24 20:06:10 -0300

    refactor(products.mapper): change name of propierties

--- src/features/favorites/mappers/products.mapper.ts
+++ src/features/favorites/mappers/products.mapper.ts
@@ -3,13 +3,13 @@ import { mapNutriments } from "./nutriments.mapper";
 
 export function mapProduct(raw: any): Product {
   return {
-    code: raw.id,
+    code: raw.code,
     productName: raw.product_name,
     ecoscoreGrade: raw.ecoscore_grade,
     ingredientsText: raw.ingredients_text,
     novaGroup: raw.nova_group,
     nutriments: mapNutriments(raw.nutriments),
     nutriscoreGrade: raw.nutriscore_grade,
-    nutriotionalDataPer: raw.nutriscore_grade,
+    nutriotionalDataPer: raw.nutrition_data_per,
   };
 }
