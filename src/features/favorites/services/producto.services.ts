4ab8d06b7d1120e1b723547f2ee423631b69ca0f
Cristian <cristian.mg.david@gmail.com>
2026-06-24 17:20:46 -0300

    feat(producto.services): add parts of the url and query and buil it

--- src/features/favorites/services/producto.services.ts
+++ src/features/favorites/services/producto.services.ts
@@ -1,7 +1,12 @@
 import { apiGet } from '@/lib/api/client';
 
-const url = 'api/v2/product/5449000205520?fields=product_name,nova_group,nutriscore_grade,ecoscore_grade,nutriments,selected_images,ingredients_text';
+const url = 'api/v2/product/';
+const query = '?fields=product_name,nova_group,nutriscore_grade,ecoscore_grade,nutriments,selected_images,ingredients_text';
 
-export async function fetchProducts() {
-  return apiGet<any>(url);
+function builUrl(code: number): string {
+  return `${url}${code}${query}`
+};
+
+export async function fetchProduct(code: number) {
+  return apiGet<any>(builUrl(code));
 }
