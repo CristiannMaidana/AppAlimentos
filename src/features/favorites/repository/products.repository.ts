8798c83f2eb7fe3138cd52dca0c692ff34488237
Cristian <cristian.mg.david@gmail.com>
2026-06-24 20:05:22 -0300

    feat(products.repository): simplify function

--- src/features/favorites/repository/products.repository.ts
+++ src/features/favorites/repository/products.repository.ts
@@ -3,6 +3,7 @@ import { fetchProduct } from "../services/producto.services";
 import { Product } from "../types/product";
 
 export async function getProduct(code:number): Promise<Product>{
-    const data = await fetchProduct(5449000205520); // Prueba de un producto: Sprite
-    return data.map(mapProduct);
+    const data = await fetchProduct(code);
+    return mapProduct(data.product);
 }
+ 
