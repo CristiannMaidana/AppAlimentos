2ba64558f25a14a48dbc6755d6282e67bfab3be2
Cristian <cristian.mg.david@gmail.com>
2026-06-26 20:58:48 -0300

    feat(search.services): change parameters of api

--- src/features/search/services/search.services.ts
+++ src/features/search/services/search.services.ts
@@ -1,7 +1,7 @@
 import { apiGet } from "@/lib/api/client";
 
 const query='cgi/search.pl?search_terms='
-const parameters='&search_simple=1&action=process&json=1&fields=code,product_name,brands,image_url'
+const parameters='&search_simple=1&action=process&json=1&fields=code,product_name,brands,image_url,nutriscore_grade,ecoscore_grade'
 
 function buildUrl(product: string): string {
     return `${query}${product}${parameters}`;
