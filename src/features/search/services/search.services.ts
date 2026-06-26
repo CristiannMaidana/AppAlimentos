f658e9c74b2b3e95a1dae9878416a7720d8ce7b8
Cristian <cristian.mg.david@gmail.com>
2026-06-26 20:44:47 -0300

    feat(search.services): create file for the request of the api

---
+++ src/features/search/services/search.services.ts
@@ -0,0 +1,12 @@
+import { apiGet } from "@/lib/api/client";
+
+const query='cgi/search.pl?search_terms='
+const parameters='&search_simple=1&action=process&json=1&fields=code,product_name,brands,image_url'
+
+function buildUrl(product: string): string {
+    return `${query}${product}${parameters}`;
+}
+
+export async function searchProduct (product: string): Promise <any> {
+    return apiGet<any>(buildUrl(product));
+}
