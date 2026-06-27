2966098568a13d2b229e307607c3914e635189f5
Cristian <cristian.mg.david@gmail.com>
2026-06-27 17:13:33 -0300

    refactor(favorites): change name of files

--- src/features/favorites/repository/products.repository.ts
+++
@@ -1,8 +0,0 @@
-import { mapProduct } from "../mappers/products.mapper";
-import { fetchProduct } from "../services/producto.services";
-import { Product } from "../types/product";
-
-export async function getProduct(code: string): Promise<Product>{
-    const data = await fetchProduct(code);
-    return mapProduct(data.product);
-}
