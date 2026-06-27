d58e5999f6568c5b0dc1808f5ec169f31b6a2b14
Cristian <cristian.mg.david@gmail.com>
2026-06-26 22:50:15 -0300

    feat(products.mapper): add propiertie

--- src/features/favorites/mappers/products.mapper.ts
+++ src/features/favorites/mappers/products.mapper.ts
@@ -4,6 +4,7 @@ import { mapNutriments } from "./nutriments.mapper";
 export function mapProduct(raw: any): Product {
   return {
     code: raw.code,
+    brands: raw.brands,
     productName: raw.product_name,
     ecoscoreGrade: raw.ecoscore_grade,
     ingredientsText: raw.ingredients_text,
