94a7c41130168047ef54be1e6792f7c041e37d92
Cristian <cristian.mg.david@gmail.com>
2026-06-26 22:40:56 -0300

    feat(products.repository): change type of data in paramets

--- src/features/favorites/repository/products.repository.ts
+++ src/features/favorites/repository/products.repository.ts
@@ -2,8 +2,7 @@ import { mapProduct } from "../mappers/products.mapper";
 import { fetchProduct } from "../services/producto.services";
 import { Product } from "../types/product";
 
-export async function getProduct(code:number): Promise<Product>{
+export async function getProduct(code: string): Promise<Product>{
     const data = await fetchProduct(code);
     return mapProduct(data.product);
 }
- 
