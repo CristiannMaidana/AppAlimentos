bb92f78052bbed75c013f3589a6d5b6ced3f874c
Cristian <cristian.mg.david@gmail.com>
2026-06-26 22:38:13 -0300

    feat(producto.services): change the type of data on paramets

--- src/features/favorites/services/producto.services.ts
+++ src/features/favorites/services/producto.services.ts
@@ -3,10 +3,10 @@ import { apiGet } from '@/lib/api/client';
 const url = 'api/v2/product';
 const query = '?fields=code,product_name,nova_group,nutriscore_grade,ecoscore_grade,nutriments,selected_images,ingredients_text,nutrition_data_per';
 
-function buildUrl(code: number): string {
+function buildUrl(code: string): string {
   return `${url}/${code}${query}`;
 }
 
-export async function fetchProduct(code: number): Promise<any> {
+export async function fetchProduct(code: string): Promise<any> {
   return apiGet<any>(buildUrl(code));
 }
