87bf717e15fbd1c623f7b51c3b62bc9b5af1fbb4
Cristian <cristian.mg.david@gmail.com>
2026-07-04 00:16:31 -0300

    feat(camera-screen): create page for the camera

---
+++ src/features/camera/camera-screen.tsx
@@ -0,0 +1,63 @@
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function CameraScreen() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} />
+      <View style={styles.buttonContainer}>
+        <Text>Escanee un producto para ver detalles.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    padding: 16,
+    position: 'absolute',
+    bottom: 64,
+    left: 30,
+    right: 30,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 24,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
