a40f144c079a77c6aaf0ed3f3c371f544d413cdc
Cristian <cristian.mg.david@gmail.com>
2026-07-04 01:05:46 -0300

    refactor(search.services): add limit of 10 items to request

--- src/features/search/services/search.services.ts
+++ src/features/search/services/search.services.ts
@@ -1,7 +1,7 @@
 import { apiGet } from '@/lib/api/client';
 
 const query = 'cgi/search.pl?search_terms=';
-const parameters = '&search_simple=1&action=process&json=1&fields=code,product_name,brands,image_url,nutriscore_grade,ecoscore_grade';
+const parameters = '&search_simple=1&action=process&json=1&fields=code,product_name,brands,image_url,nutriscore_grade,ecoscore_grade&page_size=10';
 
 function buildUrl(product: string): string {
   return `${query}${encodeURIComponent(product.trim())}${parameters}`;
