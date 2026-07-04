5c7f08d48dc62bb997750fa030ce2611b703493c
Cristian <cristian.mg.david@gmail.com>
2026-07-04 01:24:59 -0300

    feat(product.search.repository): add parameter for size of page

--- src/features/search/repository/product.search.repository.ts
+++ src/features/search/repository/product.search.repository.ts
@@ -2,7 +2,7 @@ import { mapProductsSimply } from '../mapper/product.search.mapper';
 import { searchProduct } from '../services/search.services';
 import { ProductSimply } from '../types/product.search';
 
-export async function getProductSimply(product: string): Promise<ProductSimply[]> {
-  const data = await searchProduct(product);
+export async function getProductSimply(product: string, page = 1, pageSize = 10): Promise<ProductSimply[]> {
+  const data = await searchProduct(product, page, pageSize);
   return mapProductsSimply(data.products ?? []);
 }
