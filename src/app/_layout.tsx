c428adc80429654f39c8afa6e8dcf936899abbdf
Cristian <cristian.mg.david@gmail.com>
2026-07-04 17:18:28 -0300

    refactor(_layout): change header options

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,7 +10,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{
+        headerBackButtonDisplayMode: 'minimal',
+        headerTitle: '',
+      }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
