bc6930e64406c87662da0c1be9255f873ae99953
Cristian <cristian.mg.david@gmail.com>
2026-07-10 19:32:32 -0300

    feat(favorites-screen): create new screen

---
+++ src/features/favorites/favorites-screen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from "react-native";
+
+export default function FavoritesScreen () {
+    return (
+        <View style={styles.screenBackgroundFavorites}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screenBackgroundFavorites : {
+        flex: 1,
+    }
+});
