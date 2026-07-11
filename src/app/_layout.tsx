026e2f9a08662b9005140cac43a2fbd7785f7911
Cristian <cristian.mg.david@gmail.com>
2026-07-11 19:56:44 -0300

    feat(_layout): add global provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,19 +4,22 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { ConvexClientProvider } from '@/providers/ConvexClientProvider';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{
-        headerBackButtonDisplayMode: 'minimal',
-        headerTitle: '',
-      }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      <ConvexClientProvider>
+        <Stack screenOptions={{
+          headerBackButtonDisplayMode: 'minimal',
+          headerTitle: '',
+        }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+      </ConvexClientProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
