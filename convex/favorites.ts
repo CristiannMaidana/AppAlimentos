3cce67782c9d275de902c86079a2f0bee3a618c1
Cristian <cristian.mg.david@gmail.com>
2026-07-11 19:15:01 -0300

    feat(favorites): create mutation for table favorites

---
+++ convex/favorites.ts
@@ -0,0 +1,53 @@
+import { v } from 'convex/values';
+import { mutation } from './_generated/server';
+
+export const addFavorite = mutation ({
+    args: {
+        code: v.string(),
+        brands: v.string(),
+        ecoscoreGrade: v.string(),
+        imageUrl: v.optional(v.string()),
+        nutriscoreGrade: v.string(),
+        productName: v.string(),
+    },
+    handler: async (ctx, args) => {
+        // Check if already exist the product
+        const existingFavorite = await ctx.db.
+        query("favorites").
+        withIndex("by_code", (query) => 
+            query.eq("code", args.code),
+        ).unique();
+        
+        if (existingFavorite) {
+            return existingFavorite._id;
+        }
+
+        await ctx.db.insert("favorites", {
+            code: args.code,
+            brands: args.brands,
+            ecoscoreGrade: args.ecoscoreGrade,
+            imageUrl: args.imageUrl,
+            nutriscoreGrade: args.nutriscoreGrade,
+            productName: args.productName,
+        });
+    }
+})
+
+export const removeFavorite = mutation ({
+    args: {
+        code: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const favorite = await ctx.db
+        .query("favorites")
+        .withIndex("by_code", (query) => 
+            query.eq("code", args.code)
+        ).unique();
+        if (!favorite) {
+            return null;
+        }
+        await ctx.db.delete(favorite._id);
+
+        return favorite._id;
+    }
+})
