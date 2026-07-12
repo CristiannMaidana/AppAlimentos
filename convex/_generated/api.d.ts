fcf0cb4bcf502bc6fc5350dd6e5aeda9341e137c
Cristian <cristian.mg.david@gmail.com>
2026-07-12 15:07:32 -0300

    feat(api.d): add new file

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as auth from "../auth.js";
 import type * as favorites from "../favorites.js";
 
 import type {
@@ -17,6 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
   favorites: typeof favorites;
 }>;
 
